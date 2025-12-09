b77e5591db0b921bd6ec8ba5193e48c330399cfd
ilcann <ilcan21@itu.edu.tr>
2025-12-10 01:10:09 +0300

    Update package version to 1.1.0 and enhance type exports

--- tsup.config.ts
+++ tsup.config.ts
@@ -4,6 +4,7 @@ export default defineConfig({
   entry: {
     index: "src/index.ts",
     "decorators/index": "src/decorators/index.ts",
+    "types/index": "src/types/index.ts",
   },
   format: ["esm", "cjs"],
   dts: true,
